090f3ccf61f5641cd68b1d5199fed1c74f8da1fc
emily0622 <ejtraynor2001@gmail.com>
2022-01-29 17:31:17 -0500

    working but not fast enough

---
+++ ROB311/breadth_first_search.py
@@ -0,0 +1,60 @@
+# Be sure to compare with breadth_first_search!
+import queue #IS THIS RIGHT??!!!
+import numpy as np
+from search_problems import Node, GraphSearchProblem
+
+
+def breadth_first_search(problem):
+    """
+    Implement a simple breadth-first search algorithm that takes instances of SimpleSearchProblem (or its derived
+    classes) and provides a valid and optimal path from the initial state to the goal state. Useful for testing your
+    bidirectional and A* search algorithms.
+
+    :param problem: instance of SimpleSearchProblem
+    :return: path: a list of states (ints) describing the path from problem.init_state to problem.goal_state[0]
+             num_nodes_expanded: number of nodes expanded by your search
+             max_frontier_size: maximum frontier size during search
+    """
+    ####
+    #   COMPLETE THIS CODE
+    ####
+    max_frontier_size = 0
+    num_nodes_expanded = 0
+    path = []
+    return path, num_nodes_expanded, max_frontier_size
+
+
+if __name__ == '__main__':
+    # Simple example
+    goal_states = [0]
+    init_state = 9
+    V = np.arange(0, 10)
+    E = np.array([[0, 1],
+                  [1, 2],
+                  [2, 3],
+                  [3, 4],
+                  [4, 5],
+                  [5, 6],
+                  [6, 7],
+                  [7, 8],
+                  [8, 9],
+                  [0, 6],
+                  [1, 7],
+                  [2, 5],
+                  [9, 4]])
+    problem = GraphSearchProblem(goal_states, init_state, V, E)
+    path, num_nodes_expanded, max_frontier_size = breadth_first_search(problem)
+    correct = problem.check_graph_solution(path)
+    print("Solution is correct: {:}".format(correct))
+    print(path)
+
+    # Use stanford_large_network_facebook_combined.txt to make your own test instances
+    E = np.loadtxt('../datasets/stanford_large_network_facebook_combined.txt', dtype=int)
+    V = np.unique(E)
+    goal_states = [349]
+    init_state = 0
+    problem = GraphSearchProblem(goal_states, init_state, V, E)
+    path, num_nodes_expanded, max_frontier_size = breadth_first_search(problem)
+    correct = problem.check_graph_solution(path)
+    print("Solution is correct: {:}".format(correct))
+    print(path)
